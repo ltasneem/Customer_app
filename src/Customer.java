21f08add13fa5a37dcdf03bdcd14cce111c5d714
Lubaba <lubaba070@gmail.com>
2017-07-07 11:21:05 -0400

    insertion done

--- src/Customer.java
+++ src/Customer.java
@@ -1,8 +1,86 @@
 
 public class Customer {
 	
+	String fullname;
+	String saddress;
+	String city;
+	String state;
+	String zip;
+	
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getSaddress() {
+		return saddress;
+	}
+
+	public void setSaddress(String saddress) {
+		this.saddress = saddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public Customer(String fname,String address, String c_city,String c_state,String c_zip)
+	{
+		fullname=fname;
+		saddress=address;
+		city=c_city;
+		state=c_state;
+		zip=c_zip;
+	}
+	
 	public Customer()
 	{
 		
 	}
+	
+	public String getInfo(String lname)
+	{
+		return "select * from customers where lastname ='"+lname+"'";
+	}
+	
+	public String update_address(String lastname,String address)
+	{
+		
+		return "update customers c set c.StreetAddress = '"+address+"' where c.lastname ='"+lastname+"'";
+	}
+	
+	public String insert_entry(Customer cus)
+	{
+		System.out.println(cus.getFullname());
+		String []splitted = cus.getFullname().split(" ");
+		String email=splitted[0]+"@gmail.com";
+		return "insert into customers values('"+cus.getFullname()+
+				"','MR.','"+splitted[0]+"','"+splitted[1]+"','"+cus.getSaddress()+"','"+
+		        cus.getCity()+"','"+cus.getState()+"','"+cus.getZip()+ "','"+email+"','position','company')";
+	}
+	
+	
 }
--- src/TestOracleJDBC.java
+++ src/TestOracleJDBC.java
@@ -10,22 +10,106 @@ public class TestOracleJDBC {
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		System.out.println("Enter the LastName");
 		Scanner in = new Scanner(System.in);
-		String input=in.next();
-		String sql = "select * from customers where lastname ='"+input+"'";
+		String choice="";
+		Customer cs= new Customer();
+		while(true)
+		{
+			System.out.println("Press (1) to search with customer's lastname or press (2) to Edit the customer's address.");
+			choice = in.nextLine();
+			if(choice.equals("q"))
+			{
+				break;
+			}
+			else if(choice.equals("1"))
+			{
+				System.out.println("Enter the LastName");
+				String input=in.nextLine();
+				String sql = cs.getInfo(input);
+				try{
+					Class.forName("oracle.jdbc.driver.OracleDriver");
+		                        //con = DriverManager.getConnection("jdbc:oracle:thin:sys as sysdba/oracle@localhost:1521:orcl");
+		                        con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+					stmt = con.createStatement();
+					rs = stmt.executeQuery(sql);
+					if(rs.next()==false)
+					{
+						break;
+						
+					}
+					while(rs.next()){
+						System.out.println(rs.getString(1) + "\t");
+						System.out.println(rs.getString(5));
+						System.out.println(rs.getString(6) + " " +rs.getString(7)+ " " +rs.getString(8));
+						System.out.println(rs.getString(9));
+					}
+				
+					}catch (SQLException e) {
+						e.printStackTrace();
+					}catch (ClassNotFoundException e) {
+						e.printStackTrace();
+				} finally {
+					try {
+						rs.close();
+						stmt.close();
+						con.close();
+					}catch(SQLException e){
+						e.printStackTrace();
+					}
+				}
+			}
+			else if(choice.equals("2"))
+			{
+				System.out.println("Enter the LastName");
+				String input=in.next();
+				System.out.println("Enter the Street Address");
+				String changed_address = in.next();
+				String sql = cs.update_address(input,changed_address);
+				try{
+					Class.forName("oracle.jdbc.driver.OracleDriver");
+		                        //con = DriverManager.getConnection("jdbc:oracle:thin:sys as sysdba/oracle@localhost:1521:orcl");
+		                        con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+					stmt = con.createStatement();
+					rs = stmt.executeQuery(sql);
+					while(rs.next()){
+						System.out.println(rs.getString(1) + "\t");
+						System.out.println(rs.getString(5));
+						System.out.println(rs.getString(6) + " " +rs.getString(7)+ " " +rs.getString(8));
+						System.out.println(rs.getString(9));
+					}
+					}catch (SQLException e) {
+						e.printStackTrace();
+					}catch (ClassNotFoundException e) {
+						e.printStackTrace();
+				} finally {
+					try {
+						rs.close();
+						stmt.close();
+						con.close();
+					}catch(SQLException e){
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+		System.out.println("Please enter Fullname,street address,city,state,zip of customer to add a new entry :");
+		in.nextLine();
+		String fullname=in.nextLine();
+		String saddress=in.nextLine();
+		String city=in.nextLine();
+		String state=in.nextLine();
+		String zip=in.nextLine();
+		
+		Customer cus = new Customer(fullname,saddress,city,state,zip);
+		String sql=cus.insert_entry(cus);
 		try{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
                         //con = DriverManager.getConnection("jdbc:oracle:thin:sys as sysdba/oracle@localhost:1521:orcl");
                         con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
 			stmt = con.createStatement();
 			rs = stmt.executeQuery(sql);
-			while(rs.next()){
-				System.out.println(rs.getString(1) + "\t");
-				System.out.println(rs.getString(5));
-				System.out.println(rs.getString(6) + " " +rs.getString(7)+ " " +rs.getString(8));
-				System.out.println(rs.getString(9));
-			}
+			System.out.println("Insertion Done");
 			}catch (SQLException e) {
 				e.printStackTrace();
 			}catch (ClassNotFoundException e) {
@@ -38,6 +122,7 @@ public class TestOracleJDBC {
 			}catch(SQLException e){
 				e.printStackTrace();
 			}
+		
 		}
 	}
 }
